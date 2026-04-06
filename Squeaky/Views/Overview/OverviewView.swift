331ba7b7d22b247b7b5f906420d94a3d62260c96
naylaabelsabathyani <naylaabelsabathyani@gmail.com>
2026-04-06 11:19:24 +0700

    commit: New page OverviewVIew

---
+++ Squeaky/Views/Overview/OverviewView.swift
@@ -0,0 +1,18 @@
+//
+//  OverviewView.swift
+//  Squeaky
+//
+//  Created by Nayla Abel Sabathyani on 06/04/26.
+//
+
+import SwiftUI
+
+struct OverviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OverviewView()
+}
