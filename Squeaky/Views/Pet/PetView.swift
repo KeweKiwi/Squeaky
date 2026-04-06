687de0d54168048bf9e184546351f5a35a74d79d
elvernh <87465378+elvernh@users.noreply.github.com>
2026-04-06 10:59:50 +0700

    commit: New page PetView

---
+++ Squeaky/Views/Pet/PetView.swift
@@ -0,0 +1,18 @@
+//
+//  PetView.swift
+//  Squeaky
+//
+//  Created by Christianto Elvern Haryanto on 06/04/26.
+//
+
+import SwiftUI
+
+struct PetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PetView()
+}
